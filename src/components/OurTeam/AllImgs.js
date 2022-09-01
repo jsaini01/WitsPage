9129c9ed2583025e83f7b38a23ddbbf3822c85a6
jsaini01 <jaspreet.singh@thewitslab.com>
2022-09-01 13:46:33 +0530

    photosabsolute

--- src/components/OurTeam/AllImgs.js
+++ src/components/OurTeam/AllImgs.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { AllImages, BackImg, Animator } from "../../styles/OurTeamStyle";
+import {
+  AllImages,
+  BackImg,
+  Animator,
+  Ellipse,
+  JuliaCenter,
+  Employee200,
+  Ellipse1934,
+  Ellipse1935,
+  UxDesign,
+  Ellipse1940,
+  Ellipse1939,
+  Ellipse1938,
+} from "../../styles/OurTeamStyle";
 
 const AllImgs = () => {
   return (
     <AllImages>
       <BackImg src="./images/backimg.png" alt="backimg" />
+      <JuliaCenter src="./images/julia-center-with.png" alt="julia" />
       <Animator src="./images/animator.png" alt="animator" />
+      <Ellipse src="./images/ellipse-1937.png" alt="elli1937" />
+      <Employee200 src="./images/employe200.png" alt="employee" />
+      <Ellipse1934 src="./images/ellipse-1934.png" alt="ellipse1934" />
+      <Ellipse1935 src="./images/ellipse-1935.png" alt="ellipse1935" />
+      <UxDesign src="./images/uxdesigner.png" alt="uxdesign" />
+      <Ellipse1940 src="./images/ellipse-1940.png" alt="ellipse1940" />
+      <Ellipse1939 src="./images/ellipse-1939.png" alt="ellipse1939" />
+      <Ellipse1938 src="./images/ellipse-1938.png" alt="ellipse1938" />
     </AllImages>
   );
 };
--- src/styles/OurTeamStyle.js
+++ src/styles/OurTeamStyle.js
@@ -46,12 +46,75 @@ export const AllImages = styled.div`
 `;
 
 export const BackImg = styled.img`
-  width: calc(100% - 30%);
+  width: calc(100% - 31.97%);
 `;
 
 export const Animator = styled.img`
   position: absolute;
   right: clamp(20rem, 42.5vw, 60rem);
   top: clamp(0.5rem, 0.6vw, 2rem);
-  width: calc(100% - 90.8%);
+  width: calc(100% - 90.7%);
+`;
+
+export const Ellipse = styled.img`
+  position: absolute;
+  right: clamp(14rem, 29.7vw, 60rem);
+  top: clamp(2.3rem, 3.7vw, 10rem);
+  width: calc(100% - 89.4%);
+`;
+
+export const JuliaCenter = styled.img`
+  position: absolute;
+  right: clamp(16.65rem, 33.8vw, 60rem);
+  top: clamp(5.4rem, 11vw, 30rem);
+  width: calc(100% - 72.1%);
+`;
+
+export const Employee200 = styled.img`
+  position: absolute;
+  right: clamp(10.8rem, 23.7vw, 60rem);
+  top: clamp(7.3rem, 14.1vw, 20rem);
+  width: calc(100% - 92.15%);
+`;
+
+export const Ellipse1934 = styled.img`
+  position: absolute;
+  right: clamp(6rem, 12.8vw, 20rem);
+  top: clamp(11.36rem, 22.2vw, 30rem);
+  width: calc(100% - 87.3%);
+`;
+
+export const Ellipse1935 = styled.img`
+  position: absolute;
+  right: clamp(11.5rem, 25vw, 30rem);
+  top: clamp(16.7rem, 34.7vw, 40rem);
+  width: calc(100% - 87.57%);
+`;
+
+export const UxDesign = styled.img`
+  position: absolute;
+  right: clamp(19.1rem, 42.8vw, 50rem);
+  top: clamp(19.81rem, 41vw, 50rem);
+  width: calc(100% - 90.9%);
+`;
+
+export const Ellipse1940 = styled.img`
+  position: absolute;
+  left: clamp(15rem, 31.9vw, 50rem);
+  top: clamp(16.3rem, 35.45vw, 50rem);
+  width: calc(100% - 88.6%);
+`;
+
+export const Ellipse1939 = styled.img`
+  position: absolute;
+  left: clamp(11.1rem, 22.9vw, 50rem);
+  top: clamp(10.6rem, 22vw, 50rem);
+  width: calc(100% - 86.95%);
+`;
+
+export const Ellipse1938 = styled.img`
+  position: absolute;
+  left: clamp(9.32rem, 19.2vw, 50rem);
+  top: clamp(2rem, 5.3vw, 50rem);
+  width: calc(100% - 86.02%);
 `;
